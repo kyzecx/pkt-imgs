97354799d41b7577f4eb0655c9b75a11a7ce8008
giant <1191467250@qq.com>
2026-02-10 01:08:37 +0800

    修复node funcction 语法错误

--- edge-functions/[[default]].js
+++ edge-functions/[[default]].js
@@ -1,10 +1,23 @@
 export default async function onRequest(context) {
-  const response = await context.next();
-  const headers = new Headers(response.headers);
+  const request = context.request;
+  const method = request.method || "GET";
+  const headers = new Headers();
+
   headers.set("Access-Control-Allow-Origin", "*");
-  return new Response(response.body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers,
+  headers.set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS");
+  headers.set("Access-Control-Allow-Headers", "*");
+
+  if (method === "OPTIONS") {
+    return new Response(null, { status: 204, headers });
+  }
+
+  const baseResponse = await fetch(request);
+  const responseHeaders = new Headers(baseResponse.headers);
+  headers.forEach((value, key) => responseHeaders.set(key, value));
+
+  return new Response(baseResponse.body, {
+    status: baseResponse.status,
+    statusText: baseResponse.statusText,
+    headers: responseHeaders,
   });
 }
